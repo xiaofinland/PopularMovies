017f87467f364cccff8447691eb670fbcfd3d8f2
xiaofinland <xiao.ma@3dreams.fi>
2016-01-07 23:32:45 +0200

    add provider

---
+++ app/src/main/java/com/example/android/popularmovie/data/FavouriteProvider.java
@@ -0,0 +1,163 @@
+package com.example.android.popularmovie.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+
+
+/**
+ * Created by Xiao on 22/12/2015.
+ */
+public class FavouriteProvider extends ContentProvider {
+    // The URI Matcher used by this content provider.
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private MovieDbHelper mOpenHelper;
+
+    static final int FAVOURITE = 100;
+    static final int TRAILER = 200;
+
+
+    static UriMatcher buildUriMatcher(){
+        //matcher is for switch function to find right table
+        //.NO_MATCH is typical way to have clean matcher
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = MovieContract.CONTENT_AUTHORITY;
+
+        matcher.addURI(authority,MovieContract.PATH_FAVOURITE,FAVOURITE);
+
+
+        return matcher;
+    }
+
+    @Override
+    public boolean onCreate(){
+        mOpenHelper = new MovieDbHelper(getContext());
+        return true;
+    }
+    @Override
+    public String getType (Uri uri){
+        final int match =sUriMatcher.match(uri);
+        switch (match){
+            case FAVOURITE:
+                return MovieContract.FavouriteEntry.CONTENT_TYPE;
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+    }
+    @Override
+    public Cursor query (Uri uri,String[] projection, String selection, String[] selectionArgs,
+                         String sortOrder) {
+        Cursor retCursor;
+        switch (sUriMatcher.match(uri)) {
+            case FAVOURITE: {
+                retCursor = mOpenHelper.getReadableDatabase().query(
+                        MovieContract.FavouriteEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            }
+
+            default:
+                throw new UnsupportedOperationException("unknown uri: " + uri);
+        }
+        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return retCursor;
+    }
+    @Override
+    public Uri insert (Uri uri, ContentValues values) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        Uri returnUri;
+
+        switch (match) {
+            case FAVOURITE: {
+                long _id = db.insert(MovieContract.FavouriteEntry.TABLE_NAME, null, values);
+                if (_id > 0)
+                    returnUri = MovieContract.FavouriteEntry.buildFavouriteUri(_id);
+                else
+                    throw new android.database.SQLException("Failed to inset row into " + uri);
+                break;
+            }
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return returnUri;
+    }
+    @Override
+    public int delete(Uri uri, String selection, String[]selectionArgs){
+        final SQLiteDatabase db =mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        int rowsDeleted;
+        // this makes delete all rows return the number of rows deleted
+        if (null == selection) selection= "1";
+        switch (match){
+            case FAVOURITE:
+                rowsDeleted = db.delete(MovieContract.FavouriteEntry.TABLE_NAME,selection,selectionArgs);
+                break;
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri " + uri);
+        }
+        // Because a null deletes all rows
+        if (rowsDeleted !=0){
+            getContext().getContentResolver().notifyChange(uri,null);
+        }
+        return rowsDeleted;
+    }
+    @Override
+    public int update (Uri uri, ContentValues values, String selection, String[]selectionArgs){
+        final SQLiteDatabase db =mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        int rowsUpdated;
+
+        switch (match){
+            case FAVOURITE:
+                rowsUpdated = db.update(MovieContract.FavouriteEntry.TABLE_NAME, values, selection, selectionArgs);
+                break;
+         
+            default:
+                throw new UnsupportedOperationException("Unknown Uri "+ uri);
+        }
+        if (rowsUpdated != 0){
+            getContext().getContentResolver().notifyChange(uri,null);
+        }
+        return rowsUpdated;
+    }
+    @Override
+    public int bulkInsert (Uri uri, ContentValues [] values){
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        switch (match){
+            case FAVOURITE:
+                db.beginTransaction();
+                int returnCount = 0;
+                try {
+                    for (ContentValues value : values){
+                        long _id = db.insert(MovieContract.FavouriteEntry.TABLE_NAME,null,value);
+                        if (_id !=-1){
+                            returnCount++;
+                        }
+                    }
+                    db.setTransactionSuccessful();
+                }finally {
+                    db.endTransaction();
+                }
+                getContext().getContentResolver().notifyChange(uri,null);
+                return returnCount;
+            default:
+                return super.bulkInsert(uri, values);
+        }
+    }
+}
