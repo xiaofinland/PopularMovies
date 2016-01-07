6d2d0cde2a014576606f2796b6c9a6eaa4932a14
xiaofinland <xiao.ma@3dreams.fi>
2016-01-07 23:04:37 +0200

    add Contract

---
+++ app/src/main/java/com/example/android/popularmovie/data/MovieContract.java
@@ -0,0 +1,37 @@
+package com.example.android.popularmovie.data;
+
+        import android.content.ContentResolver;
+        import android.content.ContentUris;
+        import android.net.Uri;
+        import android.provider.BaseColumns;
+
+/**
+ * Created by Xiao on 22/12/2015.
+ */
+public class MovieContract {
+    public static final String CONTENT_AUTHORITY ="com.example.android.popularmovie.app";
+    // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+    // the content provider.
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    // Possible paths (appended to base content URI for possible URI's)
+
+    public static final String PATH_FAVOURITE = "favourite";
+
+
+    //Inner class that defines the table contents of the favourite table
+    public static final class FavouriteEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_FAVOURITE).build();
+
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE+ "/" + CONTENT_AUTHORITY + "/" + PATH_FAVOURITE;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_FAVOURITE;
+
+        public static final  String TABLE_NAME = "favourite";
+        public static final String COLUMN_MOVIE_ID = "movie_id";
+
+        public static Uri buildFavouriteUri (Long id){
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+
+}
